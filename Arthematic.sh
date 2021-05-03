da802cf3ba765813a43848f5eb194508544a588b
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-05-03 19:19:26 +0530

    [javeed] modified and added the addition part

--- Arthematic.sh
+++ Arthematic.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-
+read -p "enter the number = " a
+read -p "enter the number = " b
+read -p "enter the number = " c
+addition=$(( a + b + c ))
+echo "add of the given numbers is = " $addition
